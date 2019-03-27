c864512d55533a04f39bcea779abf47c5b0cbf16
chinajianshen <281349838@qq.com>
2019-03-27 14:08:03 +0800

    NetCore学习及Microsoft Sample

---
+++ OpenBook.NetCoreStudy/OpenBook.WebCoreApp/Infrastructure/RequestSetOptionsMiddleware.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenBook.WebCoreApp.Infrastructure
+{
+    public class RequestSetOptionsMiddleware
+    {
+        private readonly RequestDelegate _next;
+        
+    }
+}
---
+++ OpenBook.NetCoreStudy/OpenBook.WebCoreApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OpenBook.WebCoreApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
